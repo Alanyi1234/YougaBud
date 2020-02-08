31f777f1cd51d9a7beef6997409944d52e8ffb33
Lisa Zheng <lisazheng123@gmail.com>
2020-02-08 16:37:29 -0500

    linked create new account to feed

--- andriod app test/app/src/main/java/com/example/myapplication/NewUserActivity.java
+++ andriod app test/app/src/main/java/com/example/myapplication/NewUserActivity.java
@@ -2,13 +2,29 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NewUserActivity extends AppCompatActivity {
+    private Button createAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_user);
+
+        createAccountButton = findViewById(R.id.createAccountButton);
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFeedActivity();
+            }
+        });
+    }
+    public void openFeedActivity(){
+        Intent intent = new Intent(this, FeedActivity.class);
+        startActivity(intent);
     }
 }
