df40e8c52f08c0dcd881c7544701fa446b74543e
Britney Echeverria <becheverria830@gmail.com>
2020-02-08 17:52:49 -0500

    added sample pic and changed name of Application

--- andriod app test/app/src/main/java/com/example/myapplication/PoseActivity.java
+++ andriod app test/app/src/main/java/com/example/myapplication/PoseActivity.java
@@ -1,14 +1,31 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PoseActivity extends AppCompatActivity {
 
+    private Button button_back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pose);
+
+        button_back = (Button) findViewById(R.id.button5);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backToGetActiveActivity();
+            }
+        });
+    }
+
+    public void backToGetActiveActivity(){
+        Intent intent = new Intent(this, GetActiveActivity.class);
+        startActivity(intent);
     }
 }
