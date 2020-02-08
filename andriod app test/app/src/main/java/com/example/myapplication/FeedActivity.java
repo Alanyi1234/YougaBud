2d73d0619e26b8d18e29b9674216a4d7a48f5653
Lisa Zheng <lisazheng123@gmail.com>
2020-02-08 17:52:21 -0500

    Aligned the poses

--- andriod app test/app/src/main/java/com/example/myapplication/FeedActivity.java
+++ andriod app test/app/src/main/java/com/example/myapplication/FeedActivity.java
@@ -9,23 +9,25 @@ import android.widget.Button;
 
 public class FeedActivity extends AppCompatActivity {
     private Button getActiveButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.feed);
 
         getActiveButton = (Button) findViewById(R.id.GetActiveButton);
+
         getActiveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openGetActiveActivity();
             }
         });
+
     }
 
     public void openGetActiveActivity(){
         Intent intent = new Intent(this, GetActiveActivity.class);
         startActivity(intent);
     }
+
 }
