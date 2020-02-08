fd8e9d3114899d2582deda26d79f7b1685d34731
Britney Echeverria <becheverria830@gmail.com>
2020-02-08 18:30:57 -0500

    added sample poses

--- andriod app test/app/src/main/java/com/example/myapplication/GetActiveActivity.java
+++ andriod app test/app/src/main/java/com/example/myapplication/GetActiveActivity.java
@@ -4,11 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GetActiveActivity extends AppCompatActivity {
-    private Button button_pose1;
+    private ImageButton button_pose1;
+    private ImageButton button_pose2;
+    private ImageButton button_pose3;
+    private ImageButton button_pose4;
     private Button HomeButton;
 
     @Override
@@ -16,8 +20,29 @@ public class GetActiveActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.get_active);
 
-        button_pose1 = findViewById(R.id.pose1);
+        button_pose1 = (ImageButton)findViewById(R.id.pose1);
         button_pose1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPoseActivity();
+            }
+        });
+        button_pose2 = (ImageButton)findViewById(R.id.pose2);
+        button_pose2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPoseActivity();
+            }
+        });
+        button_pose3 = (ImageButton)findViewById(R.id.pose3);
+        button_pose3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPoseActivity();
+            }
+        });
+        button_pose4 = (ImageButton)findViewById(R.id.pose4);
+        button_pose4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openPoseActivity();
