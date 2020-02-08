fc571dbaa20fc3b741a003caab9f001c11dba4c7
Lisa Zheng <lisazheng123@gmail.com>
2020-02-08 17:12:04 -0500

    Get active button linked feed to getActive page

--- andriod app test/app/src/main/java/com/example/myapplication/FeedActivity.java
+++ andriod app test/app/src/main/java/com/example/myapplication/FeedActivity.java
@@ -2,13 +2,30 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FeedActivity extends AppCompatActivity {
+    private Button getActiveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.feed);
+
+        getActiveButton = (Button) findViewById(R.id.GetActiveButton);
+        getActiveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGetActiveActivity();
+            }
+        });
+    }
+
+    public void openGetActiveActivity(){
+        Intent intent = new Intent(this, GetActiveActivity.class);
+        startActivity(intent);
     }
 }
