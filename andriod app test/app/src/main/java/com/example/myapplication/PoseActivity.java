b34f70739f6750a56b62ef104b302cb63e6f077f
Britney Echeverria <becheverria830@gmail.com>
2020-02-08 19:52:28 -0500

    connected the apps

--- andriod app test/app/src/main/java/com/example/myapplication/PoseActivity.java
+++ andriod app test/app/src/main/java/com/example/myapplication/PoseActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,11 +10,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class PoseActivity extends AppCompatActivity {
     private Button button_back;
+    private Button upload_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pose);
+
+
+        upload_button = findViewById(R.id.button3);
+        upload_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                ComponentName n = new
+                        ComponentName("org.tensorflow.lite.examples.posenet",
+                        "org.tensorflow.lite.examples.posenet.CameraActivity");
+                i.setComponent(n);
+                startActivity(i);
+            }
+        });
+
 
         button_back = findViewById(R.id.back_button_pose);
         button_back.setOnClickListener(new View.OnClickListener() {
